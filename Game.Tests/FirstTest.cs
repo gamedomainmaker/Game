ce61d22a7ce5223b0953cb121c709431fe4c3813
GameDomain Maker <gamedomainmaker@gmail.com>
2024-09-11 16:26:44 -0400

    Build passes, Tests skipped: (0/0)

--- Game.Tests/FirstTest.cs
+++ Game.Tests/FirstTest.cs
@@ -1,17 +1,7 @@
 using Game.Domain;
 
-namespace Game.Tests
+namespace Game.Tests;
+
+public class FirstTest
 {
-    public class FirstTest
-    {
-        [Fact]
-        public void OnFirstThing_Everything_ShouldReturn42()
-        {
-            var sot = new FirstThing();
-
-            var ret = sot.Everything();
-
-            Assert.Equal(42, ret);
-        }
-    }
 }
